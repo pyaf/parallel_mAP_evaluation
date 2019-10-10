107b04485cc82a115d230b7fafea014ce0e144fb
Rishabh Agrahari <rishabh.agrahari.eee15@iitbhu.ac.in>
2019-10-10 14:22:58 +0530

    Parallel implementation done

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# parallel_mAP_evaluation
-This repo parallelizes mAP_evaluation using python's multiprocessing module.
+# Parallel mAP_evaluation
+
+This repo parallelizes [mAP_evaluation](https://github.com/lyft/nuscenes-devkit/blob/master/lyft_dataset_sdk/eval/detection/mAP_evaluation.py) using python's multiprocessing module.
+
+As we know, in Lyft's 3d object detection challenge, the evaluation metric mAP is calculated as mean of mAPs for IoU thresholds 0.5, 0.55, 0.6, ... 0.95, (see [here](https://github.com/lyft/nuscenes-devkit/blob/master/lyft_dataset_sdk/eval/detection/mAP_evaluation)) looping over these 10 thresholds one by one can be time consuming process. Here's how it looks when you do so:
+
+<img src="tmp/single.png"/>
+
+Only one hyperthread is fully utilized, rest are idle.
+
+In this repo, you can find the parallelized implementation of mAP evaluation (`mAP_evaluation.py`) which uses Python's inbuilt multiprocessing module to compute APs for all 10 IoUs parallelly and simultaneously. Here's how it looks using parallelized version:
+
+<img src="tmp/parallel.png"/>
+
+The parallel implementation is ~10x faster than the for loop implementation.
+
+## Instructions
+
+As official mAP_evaluation script, this script also expects the predictions and ground truth to be in the format:
+
+`pred_file`: json file, predictions in global frame, in the format of:
+
+```
+predictions = [{
+    'sample_token': '0f0e3ce89d2324d8b45aa55a7b4f8207fbb039a550991a5149214f98cec136ac',
+    'translation': [971.8343488872263, 1713.6816097857359, -25.82534357061308],
+    'size': [2.519726579986132, 7.810161372666739, 3.483438286096803],
+    'rotation': [0.10913582721095375, 0.04099572636992043, 0.01927712319721745, 1.029328402625659],
+    'name': 'car',
+    'score': 0.3077029437237213
+}]
+
+```
+
+`gt_file`: ground truth annotations in global frame, in the format of:
+
+```
+gt = [{
+    'sample_token': '0f0e3ce89d2324d8b45aa55a7b4f8207fbb039a550991a5149214f98cec136ac',
+    'translation': [974.2811881299899, 1714.6815014457964, -23.689857123368846],
+    'size': [1.796, 4.488, 1.664],
+    'rotation': [0.14882026466054782, 0, 0, 0.9888642620837121],
+    'name': 'car'
+}]
+```
+
+`output_dir`: a directory to save the final results.
+
+I've given a sample `pred_file` and `gt_file` in the `tmp` folder of this repository.
+Here's how you can run the evaluation script:
+
+`python mAP_evaluation.py --gt_file="tmp/gt_data.json" --pred_file="tmp/pred_data.json" --output_dir="tmp/"`
+
+
+After this command finishes, you'll find metric_summary.json file in tmp containing the mAPs of all the iou thresholds as well as the overall mAP.
+
+
+Enjoy!
---
+++ mAP_evaluation.py
@@ -0,0 +1,133 @@
+'''
+Parallel 3D mAP calculation for the data in nuScenes format.
+
+File formats:
+
+`pred_file`: json file, predictions in global frame, in the format of:
+
+predictions = [{
+    'sample_token': '0f0e3ce89d2324d8b45aa55a7b4f8207fbb039a550991a5149214f98cec136ac',
+    'translation': [971.8343488872263, 1713.6816097857359, -25.82534357061308],
+    'size': [2.519726579986132, 7.810161372666739, 3.483438286096803],
+    'rotation': [0.10913582721095375, 0.04099572636992043, 0.01927712319721745, 1.029328402625659],
+    'name': 'car',
+    'score': 0.3077029437237213
+}]
+
+
+`gt_file`: ground truth annotations in global frame, in the format of:
+
+
+gt = [{
+    'sample_token': '0f0e3ce89d2324d8b45aa55a7b4f8207fbb039a550991a5149214f98cec136ac',
+    'translation': [974.2811881299899, 1714.6815014457964, -23.689857123368846],
+    'size': [1.796, 4.488, 1.664],
+    'rotation': [0.14882026466054782, 0, 0, 0.9888642620837121],
+    'name': 'car'
+}]
+
+NOTICE both are lists of dicts (annotations).
+
+'''
+
+import json
+import fire
+from pathlib import Path
+import numpy as np
+from multiprocessing import Process
+
+from lyft_dataset_sdk.eval.detection.mAP_evaluation import get_average_precisions
+
+
+def save_ap(gt, predictions, class_names, iou_threshold, output_dir):
+    ''' computes average precisions (ap) for a given threshold, and saves the metrics in a temp file '''
+    ap = get_average_precisions(gt, predictions, class_names, iou_threshold)
+    metric = {c:ap[idx] for idx, c in enumerate(class_names)}
+    summary_path = output_dir / f'metric_summary_{iou_threshold}.json'
+    with open(str(summary_path), 'w') as f:
+        json.dump(metric, f)
+
+
+def get_metric_overall_ap(iou_th_range, output_dir, class_names):
+    ''' reads temp files and calculates overall metrics, returns:
+    `metric`: a dict with key as iou thresholds and value as dicts of class and their respective APs,
+    `overall_ap`: overall ap of each class
+    '''
+
+    metric = {}
+    overall_ap = np.zeros(len(class_names))
+    for iou_threshold in iou_th_range:
+        summary_path = output_dir / f'metric_summary_{iou_threshold}.json'
+        #import pdb; pdb.set_trace()
+        with open(str(summary_path), 'r') as f:
+            data = json.load(f) # type(data): dict
+            metric[iou_threshold] = data
+            overall_ap += np.array([data[c] for c in class_names])
+        summary_path.unlink() # delete this file
+    overall_ap /= len(iou_th_range)
+    return metric, overall_ap
+
+
+def main(gt_file, pred_file, output_dir):
+    '''
+    Main function to compute mAP
+
+    args:
+    gt_file: json file path with ground truth annotations
+    pred_file: json file path with predicted annotations
+    output_dir: the final computed metrics are saved in this directory as a json file
+    '''
+
+    gt_path = Path(gt_file)
+    pred_path = Path(pred_file)
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    with open(str(pred_path)) as f:
+        predictions = json.load(f)
+
+    with open(str(gt_path)) as f:
+        gt = json.load(f)
+
+
+    class_names = ['animal', 'bicycle', 'bus', 'car', 'emergency_vehicle',
+                    'motorcycle', 'other_vehicle', 'pedestrian', 'truck']
+
+    iou_th_range = np.linspace(0.5, 0.95, 10) # 0.5, 0.55, ..., 0.90, 0.95
+
+    metric = {}
+
+    # create and start parallel processes
+    processes = []
+    for iou_threshold in iou_th_range:
+        process = Process(target=save_ap, args=(gt, predictions, class_names, iou_threshold, output_dir))
+        process.start()
+        processes.append(process)
+
+    for process in processes:
+        process.join()
+
+    # get overall metrics
+    metric, overall_ap = get_metric_overall_ap(iou_th_range, output_dir, class_names)
+
+    mAP = np.mean(overall_ap)
+    #overall_ap_dict = {c: overall_ap[idx] for idx, c in enumerate(class_names)}
+    #metric['overall'] = {c:overall_ap_dict[c] for c in sorted(class_names)}
+    metric['overall'] = {c: overall_ap[idx] for idx, c in enumerate(class_names)}
+    metric['mAP'] = mAP
+
+    summary_path = Path(output_dir) / 'metric_summary.json'
+    with open(str(summary_path), 'w') as f:
+        json.dump(metric, f, indent=4)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
+
+
+'''
+example command
+
+python mAP_evaluation.py --gt_file="tmp/gt_data.json" --pred_file="tmp/pred_data.json" --output_dir="tmp/"
+
+'''
